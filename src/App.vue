93dcb077f7c7a9788399cffbd80dfb2968a1deaf
mohammadrezaGholamipour <mrzgp2016@gmail.com>
2024-11-10 15:57:57 +0400

    set telegram config

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,29 @@
 <template>
-  <p>hello</p>
+  <div>
+    <h1>Welcome to Telegram Mini App</h1>
+    <p v-if="user">Hello, {{ user.first_name }}</p>
+    <button @click="sendData">Send Data to Telegram</button>
+  </div>
 </template>
 
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const user = ref(null);
+
+onMounted(() => {
+  if (window.Telegram && window.Telegram.WebApp) {
+    // مقداردهی اولیه API تلگرام
+    window.Telegram.WebApp.ready();
+    // گرفتن اطلاعات کاربر
+    user.value = window.Telegram.WebApp.initDataUnsafe.user;
+  }
+});
+
+const sendData = () => {
+  // ارسال داده به تلگرام
+  if (window.Telegram && window.Telegram.WebApp) {
+    window.Telegram.WebApp.sendData("Hello from Vue 3!");
+  }
+};
+</script>
